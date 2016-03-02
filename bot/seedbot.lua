69a30d6cdd881ee4831fb5282f3614487d3426a9
c9bot1380 <c9bot1380@gmail.com>
2016-03-02 20:07:21 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {147191022},--Sudo users
+    sudo_users = {147191022,120816252,161246734},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
 ]],
